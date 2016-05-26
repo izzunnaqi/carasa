bb084a2b609df81e68ced3bf2a367a7e7ec7e5cc
izzunnaqi <izzunnaqi@gmail.com>
2016-05-26 07:29:32 +0700

    blah

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => 'sql6.freemysqlhosting.net',
-            'database'  => 'sql6117053',
-            'username'  => 'sql6117053',
-            'password'  => 'ekj7pAk4F6',
+            'host'      => env('DB_HOST', ''),
+            'database'  => env('DB_DATABASE', ''),
+            'username'  => env('DB_USERNAME', ''),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
